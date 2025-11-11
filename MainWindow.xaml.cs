e551169973baeaf8cca225f0491013abb9b99be6
VladPyankov1337 <vladipyankov@yandex.ru>
2025-11-11 23:39:29 +0500

    Выполненная практическая работа на оценку удовлетворительно. Пьянков

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace WpfApp26
 {
@@ -20,9 +8,28 @@ namespace WpfApp26
     /// </summary>
     public partial class MainWindow : Window
     {
+        public static MainWindow init;
         public MainWindow()
         {
             InitializeComponent();
+            init = this;
+            OpenPages(new Pages.Clubs.Main());
+        }
+
+        private void Clubs(object sender, RoutedEventArgs e)
+        {
+            OpenPages(new Pages.Clubs.Main());
+        }
+
+        private void Users(object sender, RoutedEventArgs e)
+        {
+            OpenPages(new Pages.Users.Main());
+
+        }
+
+        public void OpenPages(Page Page)
+        {
+            frame.Navigate(Page);
         }
     }
 }
