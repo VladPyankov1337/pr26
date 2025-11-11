0afd3907d0d4610cd99b8c6ce412e67b4f7eb1c6
VladPyankov1337 <vladipyankov@yandex.ru>
2025-11-11 22:35:54 +0500

    Разработанные модели. Пьянков

---
+++ Models/Club.cs
@@ -0,0 +1,10 @@
+﻿namespace WpfApp26.Models
+{
+    public class Club
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string WorkTime { get; set; }
+    }
+}
---
+++ Models/Users.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace WpfApp26.Models
+{
+    public class Users
+    {
+        public int Id { get; set; }
+        public string FIO { get; set; }
+        public DateTime RentStart { get; set; }
+        public int Duration { get; set; }
+        public int IdClub { get; set; }
+    }
+}
